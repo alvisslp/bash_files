96e05a6931dc6be2d6a33ecc445fe65e504f7e9e
Julian COLIN <julian.colin@murex.com>
2016-04-13 11:48:31 +0200

    ShellCheck now handle exit status and absolute and relative lookup_files path

--- ShellCheck.sh
+++ ShellCheck.sh
@@ -4,22 +4,40 @@ tmp_IFS=$IFS
 IFS=';'
 read -r -a lookup_array <<< "$lookup_files"
 IFS=$tmp_IFS
+error_code=0
 
 mkdir -p ${WORKSPACE}/checkStyleResults
 
-function do_shell_check {
+exec_shell_check () {
+	if [[ "$1" != /* ]]
+	then
+		f_file="${WORKSPACE}/$1"
+	else
+		f_file=$1
+	fi
+	shellcheck $f_file -f checkstyle > ${WORKSPACE}/checkStyleResults/$(basename $f_file).xml
+	exit_code=$?
+	if [ $exit_code -eq 2 ] || [ $exit_code -eq 3 ] || [ $exit_code -eq 4  ]
+	then
+		error_code=1
+	fi
+}
+
+function launch_files_check {
 	for lookup_item in ${lookup_array[@]}
 	do
 		if [ -d $lookup_item ]
 		then
 			for file in $(find ${lookup_item}  -name "*.sh"); do 
-				shellcheck ${file} -f checkstyle > ${WORKSPACE}/checkStyleResults/$(basename ${file}).xml
+				exec_shell_check $file
 			done
 		elif [ ${lookup_item: -3} == ".sh" ]
 		then
-			shellcheck $lookup_item -f checkstyle > ${WORKSPACE}/checkStyleResults/$(basename $lookup_item).xml
+			exec_shell_check $lookup_item
 		fi
 	done
 }
 
-do_shell_check
+launch_files_check
+
+exit $error_code
